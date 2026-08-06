fcd3fc39b449801789f6c5465aff0a193d0113e7
Matthias Pinard <matthias@pixpay.fr>
2026-08-06 16:42:31 +0200

    Fix redis version

--- terraform_templates/AWS/elasticache.tf
+++ terraform_templates/AWS/elasticache.tf
@@ -32,10 +32,10 @@ resource "aws_security_group" "redis" {
 resource "aws_elasticache_cluster" "marble" {
   cluster_id           = "marble-redis"
   engine               = "redis"
-  engine_version       = "7.x"
+  engine_version       = "6.x"
   node_type            = "cache.t4g.micro"
   num_cache_nodes      = 1
-  parameter_group_name = "default.redis7"
+  parameter_group_name = "default.redis6.x"
   port                 = 6379
   subnet_group_name    = aws_elasticache_subnet_group.marble.name
   security_group_ids   = [aws_security_group.redis.id]
